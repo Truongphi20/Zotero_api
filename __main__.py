c683bd878f77304a5d07cfe6c19f7b1d613407d3
Truongphikt <phinguyen@ktest.vn>
2024-04-30 21:14:19 +0700

    update: update env

--- __main__.py
+++ __main__.py
@@ -2,34 +2,13 @@ import zotero as ztr
 import os
 
 if __name__ == "__main__":
-    API_KEY = os.environ["API_KEY"]
+    API_KEY = os.environ["ZOTERO_API_KEY"]
     USER_ID = "13591208"
     library_type = "user"
 
     auth = ztr.Auth(user_id=USER_ID, api_key=API_KEY, library_type=library_type)
 
-    # search = {
-    #     "name": "title",
-    #     "conditions": [
-    #         {
-    #             "condition": "title",
-    #             "operator": "contains",
-    #             "value": "Bioactive compounds and strategy processing for acerola: A review",
-    #         },
-    #     ],
-    # }
-
-    # print(ztr.Search(auth, search).results[1])
-
     collection_id = "5JFGIWPG"
-    data_key = "TRQNW5S6"
-
-    # print([a.get("key") for a in ztr.Collection(auth, collection_id).items])
-    # for item in ztr.Collection(auth, collection_id).items:
-    #     print(item.get('key'), item.get('data').get('contentType') ,item.get('data').get('title'))
-        # if item["key"] == data_key:
-        #     print(item)
-    # auth.zot.dump("TRQNW5S6")
 
     pdf_items = ztr.Collection(auth, collection_id).pdf_items
     ref_items = ztr.Collection(auth, collection_id).ref_items
